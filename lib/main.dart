5888d5b024edd913ca1eacf7123d289a1e553706
Leo <leooh3151@gmail.com>
2024-12-11 21:49:46 +0900

    급식받아서출력하는거 & 권한설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import "dart:convert";
 
 void main() {
   runApp(const MainApp());
@@ -21,6 +23,33 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String name = "오채승파산";
+  String gupsickUrl = "https://open.neis.go.kr/hub/mealServiceDietInfo";
+  Map gupsickData = {'ㅇ': "초기화안됨"};
+  @override
+  void initState() {
+    super.initState();
+    fetchGupsick();
+  }
+
+  void fetchGupsick() async {
+    String gupsickUrl = "https://open.neis.go.kr/hub/mealServiceDietInfo";
+
+    String apiKey = "ce7a974d58d14584ab0d35adcf0fe682";
+
+    var url = Uri.parse(gupsickUrl);
+    var finalUrl = url.replace(queryParameters: {
+      "KEY": apiKey,
+      "Type": "json",
+      "SD_SCHUL_CODE": "7130155",
+      "ATPT_OFCDC_SC_CODE": "B10",
+      "MLSV_YMD": "20241211"
+    });
+    print(finalUrl.toString());
+    var res = await http.get(finalUrl);
+    setState(() {
+      gupsickData = jsonDecode(res.body); // JsonDecoder 대신 jsonDecode 사용
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,19 +67,19 @@ class _HomeState extends State<Home> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '오Te채승파산',
+                        jsonEncode(gupsickData),
                         textAlign: TextAlign.left,
                       ),
                       Text(
-                        '오Te채승파산',
+                        '오Te파산',
                         textAlign: TextAlign.left,
                       ),
                       Text(
-                        '오Te채승파산',
+                        '오Te파산',
                         textAlign: TextAlign.left,
                       ),
                       Text(
-                        '오Te채승파산',
+                        '오Te파산',
                         textAlign: TextAlign.left,
                       ),
                     ],
