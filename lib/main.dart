f03f135c2cc4ba66c7e5bceb7ac04b371566caa3
Leo <leooh3151@gmail.com>
2024-12-11 16:29:24 +0900

    dashlfasd

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,28 @@ class _HomeState extends State<Home> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 Expanded(
-                  child: Text('오Te채승파산'),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '오Te채승파산',
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        '오Te채승파산',
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        '오Te채승파산',
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        '오Te채승파산',
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
+                  ),
                 ),
                 Image.network(
                   "https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp",
