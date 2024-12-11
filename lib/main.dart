5b20999055fd77c7b56493bc570df34bf4767ca2
Leo <leooh3151@gmail.com>
2024-12-11 15:59:24 +0900

    ddafasdfa

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,27 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String name = "심우재파산";
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World! $name'),
       ),
     );
   }
