738247040f87439c4088128b0783dd72ea98e9f7
Leo <leooh3151@gmail.com>
2024-12-11 16:23:20 +0900

    5

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class _HomeState extends State<Home> {
                 ),
                 Image.network(
                   "https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp",
+                  width: 100,
                 )
               ],
             )
